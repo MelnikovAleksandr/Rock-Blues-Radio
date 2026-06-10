0c454e180b0683334d70e5b9c3a11c50b63c54ae
MelnikovAleksandr <MelnikovJavaDev@gmail.com>
2026-06-10 21:08:50 +0500

    fix: audioAttributes

--- app/src/main/java/ru/asmelnikov/rockbluesradio/presentation/service/PlaybackService.kt
+++ app/src/main/java/ru/asmelnikov/rockbluesradio/presentation/service/PlaybackService.kt
@@ -4,6 +4,8 @@ import android.app.PendingIntent
 import android.content.Intent
 import androidx.annotation.OptIn
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MediaMetadata
 import androidx.media3.common.util.UnstableApi
@@ -45,7 +47,13 @@ class PlaybackService : MediaLibraryService() {
 
     override fun onCreate() {
         super.onCreate()
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(C.USAGE_MEDIA)
+            .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+            .build()
+
         player = ExoPlayer.Builder(this)
+            .setAudioAttributes(audioAttributes, true)
             .setHandleAudioBecomingNoisy(true)
             .build()
 
