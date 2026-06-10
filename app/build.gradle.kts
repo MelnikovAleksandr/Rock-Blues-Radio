1021701fbffab6f3f605584c3b42910db62ccc66
MelnikovAleksandr <MelnikovJavaDev@gmail.com>
2026-06-10 21:09:18 +0500

    build: version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         minSdk = 33
         targetSdk = 37
         versionCode = 1
-        versionName = "1.2"
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
